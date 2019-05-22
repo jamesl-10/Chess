a14f47e6d63335c36a47d67f1eac170302517cda
James Sanii <james@ip-172-31-8-86.ca-central-1.compute.internal>
2019-05-22 02:33:07 +0000

    Added pieces to board. Need to create method of moving said pieces, perhaps improving the pieces themselves as sprites rather than text in the JButton. Still need to create check, moving pieces, capturing, etc.

---
+++ piece.java
@@ -0,0 +1,31 @@
+class piece
+{
+	private final String pieceName;
+	
+	public piece(String pieceName)
+	{
+		this.pieceName = pieceName;
+	}
+	
+	public String name()
+	{
+		return pieceName;
+	}
+
+}
+
+class pawn extends piece
+{
+	boolean moved = false;
+	
+	public pawn(String pieceName)
+	{
+		super(pieceName);
+		// TODO Auto-generated constructor stub
+	}
+	public static void move()
+	{
+		
+	}
+	
+}
