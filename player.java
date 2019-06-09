54e17362f8ff293cb4954bf9a813b66435b32623
James-Lee1 <james.lee@ocsbstudent.ca>
2019-06-09 19:47:12 -0400

    Update player.java

--- player.java
+++ player.java
@@ -1,10 +1,18 @@
-import java.util.ArrayList;
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Dec 2018
+ * Created for: ICS4U
+ * This is the player class
+ * 
+ * This class links the board, tiles, and pieces together, and allows the
+ * pieces to be moved.
+ *
+ ****************************************************************************/
 
 public class player
 {
 	private static tile board[][];
-	private final ArrayList<piece> playerPieces = new ArrayList<piece>();
-	private final ArrayList<piece> capturedPieces = new ArrayList<piece>();
 	private static piece currentPiece = null;
 	private static int fromX = 0;
 	private static int fromY = 0;
@@ -14,19 +22,14 @@ public class player
 		this.board = board;
 	}
 	
-	public void movePiece(String s, int x, int y)
+	// Selects a piece
+	// Very important in order to move piece
+	public static void setCurrentPiece(piece p)
 	{
-		if(s.equals("\u2654"))
-		{
-			board[x][y].setText("\u2654");
-		}
+		currentPiece = p;
 	}
 	
-	public void playerPieces(piece p)
-	{
-		playerPieces.add(p);
-	}
-	
+	// Verifies if move is legal
 	public static void isValidMove(piece p, int x, int y)
 	{
 		p.verifyMove(x, y, board);
@@ -34,17 +37,21 @@ public class player
 		fromY = y;
 	}
 	
-	public static void setCurrentPiece(piece p)
-	{
-		currentPiece = p;
-	}
+	// Moves piece currently selected from function setCurrentPiece
     public static void movePiece(int toX, int toY)
     {
     	currentPiece.move(fromX, fromY, toX, toY, board);
     }
     
+    // Captures with piece selected
     public static void capturePiece(int toX, int toY)
     {
     	currentPiece.capture(fromX, fromY, toX, toY, board);
+    }
+    
+    // Castles
+    public static void castleKing(int toX, int toY)
+    {
+    	currentPiece.castle(fromX, fromY, toX, toY, board);
     }
 }
