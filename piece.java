4ff744bd33ce273e2d540f81fe5d7dacf10e0e95
James-Lee1 <james.lee@ocsbstudent.ca>
2019-06-09 19:46:50 -0400

    Update piece.java

--- piece.java
+++ piece.java
@@ -1,41 +1,82 @@
+/****************************************************************************
+ *
+ * Created by: James Lee
+ * Created on: Dec 2018
+ * Created for: ICS4U
+ * This is the piece class
+ * 
+ * Pieces include : 
+ * 
+ * - Pawn
+ * - Knight
+ * - Bishop
+ * - Rook
+ * - Queen
+ * - King
+ *
+ ****************************************************************************/
+
 import java.awt.Color;
 
 class piece
 {
+	// Properties
 	private final String pieceName;
 	private final String color;
+	private boolean moved;
 	
+	// Constructor
 	public piece(String pieceName, String color)
 	{
 		this.pieceName = pieceName;
 		this.color = color;
 	}
 	
+	// Returns name
 	public String name()
 	{
 		return pieceName;
 	}
 
+	// Returns color
 	public String color()
 	{
 		return color;
 	}
+	
+	// Moves piece
 	public void move(int fromX, int fromY, int x, int y, tile[][] board)
 	{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	// Verifies if move is possible
 	public void verifyMove(int x, int y, tile[][] board)
 	{
 		// TODO Auto-generated method stub
 	}
 
+	// Captures piece
 	public void capture(int fromX, int fromY, int toX, int toY, tile[][] board)
 	{
 		// TODO Auto-generated method stub	
 	}
+	
+	// Specific to king; castles
+	public void castle(int fromX, int fromY, int toX, int toY, tile[][] board)
+	{
+		
+	}
+	
+	// Returns whether piece was moved or not
+	public boolean wasMoved()
+	{
+		return moved;
+	}
 }
 
+// Pawn class
 class pawn extends piece
 {
 	boolean moved = false;
@@ -46,8 +87,14 @@ class pawn extends piece
 		// TODO Auto-generated constructor stub
 	}
 	
+	// Pawn can move in these patterns :
+	// - Can move one space directly up
+	// - MUST capture diagonally one space
+	// - Can move two squares on first move
 	public void verifyMove(int x, int y, tile[][] board)
 	{
+		// White pawns
+		// Moves up one square
 		if (this.color().equals("White"))
 		{
 			try
@@ -56,6 +103,7 @@ class pawn extends piece
 				{
 					board[x-1][y].setText("\u25E6");
 					
+					// Moves up two squares
 					if (moved == false)
 					{
 						if (board[x-2][y].getText().equals(""))
@@ -67,6 +115,7 @@ class pawn extends piece
 			}
 			catch(Exception e) {}
 			
+			// Captures diagonally to the right
 			try
 			{
 				if (!board[x-1][y+1].getPiece().color().equals(this.color()))
@@ -76,6 +125,7 @@ class pawn extends piece
 			}
 			catch (Exception e) {}
 			
+			// Captures diagonally to the right
 			try
 			{
 				if (!board[x-1][y-1].getPiece().color().equals(this.color()))
@@ -86,14 +136,17 @@ class pawn extends piece
 			catch (Exception e) {}
 		}
 		
+		// Black pawns
 		else
 		{
+			// Moves up one square
 			try
 			{
 				if (board[x+1][y].getText().equals(""))
 				{
 					board[x+1][y].setText("\u25E6");
 					
+					// Moves up two squares
 					if (moved == false)
 					{
 						if (board[x+2][y].getText().equals(""))
@@ -105,6 +158,7 @@ class pawn extends piece
 			}
 			catch(Exception e) {}
 			
+			// Captures diagonally to the right
 			try
 			{
 				if (!board[x+1][y+1].getPiece().color().equals(this.color()))
@@ -114,6 +168,7 @@ class pawn extends piece
 			}
 			catch (Exception e) {}
 			
+			// Captures diagonally to the left
 			try
 			{
 				if (!board[x+1][y-1].getPiece().color().equals(this.color()))
@@ -131,11 +186,20 @@ class pawn extends piece
 		board[x][y].addPiece(this);
 		moved = true;
 		
-		if (x == 0 || x == 7)
+		// Make black queen if black pawn
+		
+		if (x == 0)
 		{
 			board[x][y].removePiece();
 			
 			piece promotedQueen = new queen("\u2655", this.color());
+			board[x][y].addPiece(promotedQueen);
+		}
+		
+		if (x == 7)
+		{
+			board[x][y].removePiece();
+			piece promotedQueen = new queen("\u265B", this.color());
 			board[x][y].addPiece(promotedQueen);
 		}
 	}
@@ -146,11 +210,17 @@ class pawn extends piece
 		board[x][y].removePiece();
 		board[x][y].addPiece(this);
 		
-		if (x == 0 || x == 7)
+		if (x == 0)
 		{
 			board[x][y].removePiece();
 			
 			piece promotedQueen = new queen("\u2655", this.color());
+			board[x][y].addPiece(promotedQueen);
+		}
+		if (x == 7)
+		{
+			board[x][y].removePiece();
+			piece promotedQueen = new queen("\u265B", this.color());
 			board[x][y].addPiece(promotedQueen);
 		}
 	}
@@ -163,8 +233,13 @@ class knight extends piece
 		super(pieceName, color);
 		// TODO Auto-generated constructor stub
 	}
+	
+	// Knight can move in these patterns :
+	// - Two squares any direction, then one square (in an L shape)
+	
 	public void verifyMove(int x, int y, tile[][] board)
 	{
+
 		try
 		{
 			if (board[x-2][y-1].getText().equals(""))
@@ -293,6 +368,9 @@ class bishop extends piece
 		// TODO Auto-generated constructor stub
 	}
 	
+	// Bishop can move in these patterns :
+	// - Any number of squares diagonally
+	
 	public void verifyMove(int x, int y, tile[][] board)
 	{
 		for (int i = 1; i < 8; i++)
@@ -405,6 +483,14 @@ class rook extends piece
 		// TODO Auto-generated constructor stub
 	}
 	
+	public boolean wasMoved()
+	{
+		return moved;
+	}
+	
+	// Rook can move in these patterns :
+	// - Any number of squares horizontally/vertically
+	
 	public void verifyMove(int x, int y, tile[][] board)
 	{	
 		for (int i = 1; i < 8; i++)
@@ -501,6 +587,7 @@ class rook extends piece
 	{
 		board[fromX][fromY].removePiece();
 		board[x][y].addPiece(this);
+		moved = true;
 	}
 	
 	public void capture(int fromX, int fromY, int x, int y, tile[][] board)
@@ -508,6 +595,7 @@ class rook extends piece
 		board[fromX][fromY].removePiece();
 		board[x][y].removePiece();
 		board[x][y].addPiece(this);
+		moved = true;
 	}
 	
 }
@@ -521,6 +609,10 @@ class queen extends piece
 		super(pieceName, color);
 		// TODO Auto-generated constructor stub
 	}
+	
+	// Queen can move in these patterns :
+	// - Any number of squares diagonally
+	// - Any number of squares vertically/horizontally
 	
 	public void verifyMove(int x, int y, tile[][] board)
 	{
@@ -701,6 +793,7 @@ class queen extends piece
 	{
 		board[fromX][fromY].removePiece();
 		board[x][y].addPiece(this);
+		moved = true;
 	}
 	
 	public void capture(int fromX, int fromY, int x, int y, tile[][] board)
@@ -708,6 +801,7 @@ class queen extends piece
 		board[fromX][fromY].removePiece();
 		board[x][y].removePiece();
 		board[x][y].addPiece(this);
+		moved = true;
 	}
 	
 }
@@ -721,60 +815,172 @@ class king extends piece
 		super(pieceName, color);
 		// TODO Auto-generated constructor stub
 	}
+	
+	// King can move in these patterns :
+	// - Any space 1 square away
+	
 	public void verifyMove(int x, int y, tile[][] board)
 	{
 		try
 		{
-			board[x][y-1].setText("\u25E6");
+			if (board[x][y-1].getText().equals(""))
+			{
+				board[x][y-1].setText("\u25E6");
+			}
+			else if (!board[x][y-1].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x][y-1].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x][y+1].setText("\u25E6");
+			if (board[x][y+1].getText().equals(""))
+			{
+				board[x][y+1].setText("\u25E6");
+			}
+			else if (!board[x][y+1].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x][y+1].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x-1][y].setText("\u25E6");
+			if (board[x-1][y].getText().equals(""))
+			{
+				board[x-1][y].setText("\u25E6");
+			}
+			else if (!board[x-1][y].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x-1][y].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x-1][y-1].setText("\u25E6");
+			if (board[x-1][y-1].getText().equals(""))
+			{
+				board[x-1][y-1].setText("\u25E6");
+			}
+			else if (!board[x-1][y-1].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x-1][y-1].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x-1][y+1].setText("\u25E6");
+			if (board[x-1][y+1].getText().equals(""))
+			{
+				board[x-1][y+1].setText("\u25E6");
+			}
+			else if (!board[x-1][y+1].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x-1][y+1].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x+1][y].setText("\u25E6");
+			if (board[x+1][y].getText().equals(""))
+			{
+				board[x+1][y].setText("\u25E6");
+			}
+			else if (!board[x+1][y].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x+1][y].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x+1][y-1].setText("\u25E6");
+			if (board[x+1][y-1].getText().equals(""))
+			{
+				board[x+1][y-1].setText("\u25E6");
+			}
+			else if (!board[x+1][y-1].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x+1][y-1].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
 		
 		try
 		{
-			board[x+1][y+1].setText("\u25E6");
+			if (board[x+1][y+1].getText().equals(""))
+			{
+				board[x+1][y+1].setText("\u25E6");
+			}
+			else if (!board[x+1][y+1].getPiece().color().equals(this.color()) && board[x][y].getBackground() != Color.RED)
+			{
+				board[x+1][y+1].setBackground(Color.RED);
+			}
 		}
 		catch(Exception e) {}
+		
+		try
+		{
+			if (this.moved == false)
+			{
+				if (board[x][y+3].getPiece().wasMoved() == false)
+				{
+					if (board[x][y+1].getPiece() == null && board[x][y+2].getPiece() == null)
+					{
+						board[x][y+2].setText("\u21c6");
+					}
+				}
+				
+				if (board[x][y-4].getPiece().wasMoved() == false)
+				{					
+					if (board[x][y-3].getPiece() == null && board[x][y-2].getPiece() == null && board[x][y-1].getPiece() == null)
+					{
+						board[x][y-2].setText("\u21c6");
+					}
+				}
+			}
+		}
+		catch (Exception e) {}
 	}
 	
 	public void move(int fromX, int fromY, int x, int y, tile[][] board)
 	{
 		board[fromX][fromY].removePiece();
 		board[x][y].addPiece(this);
+		moved = true;
+	}
+	
+	public void capture(int fromX, int fromY, int x, int y, tile[][] board)
+	{
+		board[fromX][fromY].removePiece();
+		board[x][y].removePiece();
+		board[x][y].addPiece(this);
+		moved = true;
+	}
+
+	// Castles with rook
+	public void castle(int fromX, int fromY, int x, int y, tile[][] board)
+	{
+		board[fromX][fromY].removePiece();
+		board[x][y].addPiece(this);
+
+		if (y == 6)
+		{
+			board[x][y-1].addPiece(board[x][y+1].getPiece());
+			board[x][y+1].removePiece();
+		}
+		else if (y == 2)
+		{
+			board[x][y+1].addPiece(board[x][y-2].getPiece());
+			board[x][y-2].removePiece();
+		}
+
+		moved = true;
 	}
 }
